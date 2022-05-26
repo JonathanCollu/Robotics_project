664b98f9007c552e25a98e6cd01a9918ccf97e90
jonny <jonnygates62@gmail.com>
2022-05-26 12:30:27 +0200

    real-world agent minor fixes

--- PiCarX_agent.py
+++ PiCarX_agent.py
@@ -23,9 +23,11 @@ if settings.draw_dist:
     plt.figure()
 
 class RealPiCarX(PiCarX):
-    def __init__(self, policy):
+    def __init__(self, policy, weights):
         self.px = Picarx()
+        state_dict = torch.load(f"exp_results/" + weights)
         self.policy = policy
+        self.policy.load_state_dict(state_dict)
         self.angular_velocity = 0 # Pay attention: with Picarx, this is the motor power
         self.angle = 0 # Pay attention: not in radians
         self.forward_vel = 1.2
@@ -37,15 +39,6 @@ class RealPiCarX(PiCarX):
         self.camera = PiCamera()
         self.camera.resolution = (360,480)
         self.camera.framerate = 24
-
-    def current_speed(self):
-        """
-        Current angular velocity of the wheel motors
-        """
-        return self.angular_velocity
-
-    def current_speed_API(self):
-        return self.angular_velocity
 
     def change_velocity(self, velocity):
         """
@@ -59,19 +52,55 @@ class RealPiCarX(PiCarX):
         self.px.set_dir_servo_angle(angle)
     
     def read_image(self):
+    	# if this one doesn't work properly, try the one below
         image = np.empty((self.camera.resolution[0] * self.camera.resolution[1] * 3,), dtype=np.uint8)
         self.camera.capture(image, 'rgb')
         image = image.reshape((self.camera.resolution[0], self.camera.resolution[1], 3))
         return image, self.camera.resolution
+    
+    '''
+    def read_image(self):
+        with PiCamera() as camera:
+            camera.resolution = (480, 360)
+            camera.framerate = 24
+            img = np.empty((480, 360, 3), dtype=np.uint8)
+            camera.capture(img, 'rgb')
+            return img.reshape((360, 480, 3))
+    '''
+    
+    def save_image(self, image, filename):
+        image = image.flip(image, axis=0)
+        image = Image.fromarray(image)
+        image.save(filename) 
 
     def detect_objects(self):
-        img, res = self.read_image()
+        img = self.read_image()
         img = np.flip(img, axis=0)
-        
-        image = Image.fromarray(img)
-        image.save('images/screenshot.png')
-
         return interpret_image("green", "blue", img)
+        
+    def move(self, movement, angle, rt):
+        # angle += 45  # test reduced actions
+        # move the robot in env and return the collected reward
+        if rt is not None:
+            if rt == 1:
+                angle = 180 - angle
+        if not movement:
+            # to avoid having the robot stuck, the "stay still" action
+            # is replaced with "go forward" (i.e. movement=1 angle=90)
+            base = self.forward_vel if angle == 90 else 0
+        else:
+            base = self.forward_vel
+        max_diff = 1.89
+        diff = abs(angle - 90) / 90 * max_diff
+        if angle > 90:
+            diff = (diff, 0)
+        elif angle < 90:
+            diff = (0, diff)
+        else:
+            diff = (0, 0)
+        v = (base[0] + diff[0], base[1] + diff[1])
+        self.change_velocity(v)
+        
     
     def act(self):
         """ TODO: Clarify how policy works. """
@@ -89,8 +118,9 @@ class RealPiCarX(PiCarX):
                 if k == 27:
                     break
                 
-                movement_prob, angles_dist = self.policy.forward(s)
-                m = movement_prob.round() #??????????????????????????????????????????????????
+                movement_prob, right_turn_prob, angles_dist = self.policy.forward(s)
+                m = movement_prob.round()
+                rt = right_turn_prob.round() 
                 a = angles_dist.argmax()
                 self.change_velocity(m)
                 self.change_angle(a)
