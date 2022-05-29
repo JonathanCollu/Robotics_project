f35b87644b95cb23a521ecb870fe02692eff89ad
riccardomajellaro <riccardomajellaro@gmail.com>
2022-05-29 02:34:14 +0200

    add arg parser in run.py

--- run.py
+++ run.py
@@ -4,27 +4,40 @@ import settings
 import time
 import matplotlib.pyplot as plt
 import torch
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-run_name', action='store', type=str, default=None)
+    parser.add_argument('-cp_name', action='store', type=str, default=None)
+    parser.add_argument('-epochs', action='store', type=int, default=1000)
+    parser.add_argument('-M', action='store', type=int, default=1)
+    parser.add_argument('-T', action='store', type=int, default=7)
+    parser.add_argument('-gamma', action='store', type=float, default=0.99)
+    parser.add_argument('-ef', action='store', type=float, default=None)
+    args = parser.parse_args()
+
     plt.ion()
     policy = ConvPolicyNet()
-    state_dict = torch.load(f"exp_results/conv4ch_base_10c_1_7_1981_weights.pt")
-    for name, param in state_dict.items():
-        if "hidden_layers.1" in name or "hidden_layers.2" in name:
-            policy.load_state_dict({name: param}, strict=False)
+    if args.cp_name is not None:
+        state_dict = torch.load(f"exp_results/{args.cp_name}_weights.pt")
+        for name, param in state_dict.items():
+            if "hidden_layers.1" in name or "hidden_layers.2" in name:
+                policy.load_state_dict({name: param}, strict=False)
     optimizer = torch.optim.Adam(policy.parameters(), lr=3e-4)
 
     value_net = ConvPolicyNet(value=True)
-    state_dict = torch.load(f"exp_results/conv4ch_base_10c_1_7_1981_weights.pt")
-    for name, param in state_dict.items():
-        if "hidden_layers.1" in name or "hidden_layers.2" in name:
-            value_net.load_state_dict({name: param}, strict=False)
+    if args.cp_name is not None:
+        state_dict = torch.load(f"exp_results/{args.cp_name}_weights.pt")
+        for name, param in state_dict.items():
+            if "hidden_layers.1" in name or "hidden_layers.2" in name:
+                value_net.load_state_dict({name: param}, strict=False)
     optimizer_v = torch.optim.Adam(value_net.parameters(), lr=1e-3)
 
-    agent = PiCarX(policy, optimizer, value_net, optimizer_v, 10)
-
+    agent = PiCarX(policy, optimizer, value_net, optimizer_v, 3)
+    
     try:
-        agent.train(1000, 1, 7, 0.99, ef=None, run_name="conv4ch_base_10c_1_7")
+        agent.train(args.epochs, args.M, args.T, args.gamma, ef=args.ef, run_name=args.run_name)
     except KeyboardInterrupt:
         print('\n\nInterrupted! Time: {}s'.format(time.time()))
 
